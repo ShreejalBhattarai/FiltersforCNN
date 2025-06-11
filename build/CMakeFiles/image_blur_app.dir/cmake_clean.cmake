7ed20a5027d7f00ed0c59a2bed75286562ed2775
Shreejal <shreejalbhattarai13@gmail.com>
2025-06-10 23:14:26 -0500

    Final code for the cmake was added, and the build is now succesful.

---
+++ build/CMakeFiles/image_blur_app.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/image_blur_app.dir/GaussianFilter.cpp.o"
+  "CMakeFiles/image_blur_app.dir/GaussianFilter.cpp.o.d"
+  "CMakeFiles/image_blur_app.dir/functions.cpp.o"
+  "CMakeFiles/image_blur_app.dir/functions.cpp.o.d"
+  "image_blur_app"
+  "image_blur_app.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/image_blur_app.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/image_blur_app.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for image_blur_app.
+# This may be replaced when dependencies are built.
